ef13298469deb601aec9c82f3de9df07f7d69f62
TimOomis <t.oomis@student.fontys.nl>
2015-06-01 09:58:40 +0200

    Configuratiewijzigingen.
    Oproepen van de logs moet er nog bij., het toevoegen van
    noodinstellingen werkt in ieder geval.

--- Groep 3/PTS4/src/pts4/gui/ConfiguratieController.java
+++ Groep 3/PTS4/src/pts4/gui/ConfiguratieController.java
@@ -6,10 +6,15 @@
 
 package pts4.gui;
 
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import pts4.database.IDatabase;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import pts4.database.SQL;
 
 /**
@@ -18,11 +23,55 @@ import pts4.database.SQL;
  */
 public class ConfiguratieController implements Initializable {
     
-    private IDatabase dbcon;
+    private SQL dbcon;
 
+    
+    @FXML
+    private TextField VoorNaamTB;
+    
+    @FXML
+    private TextField VoorLatTB;
+    
+    @FXML
+    private TextField VoorLonTB;
+    
+    @FXML
+    private Button AddBT;
+    
+    @FXML
+    private ComboBox<String> VoorzieningCB;
+        
+    
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
-        dbcon = new SQL();
+      dbcon = new SQL();
+      
+        VoorzieningCB.getItems().add("Politiebureau");
+        VoorzieningCB.getItems().add("Ziekenhuis");
+        VoorzieningCB.getItems().add("Brandweerkazerne");
+        
+    }
+    
+    @FXML
+    public void AddFaciity(ActionEvent event)
+    {
+        int noodtype = 0;
+        if(VoorzieningCB.getSelectionModel().getSelectedItem().toString().equals("Brandweerkazerne"))
+        {
+            noodtype = 1; 
+        }
+        else if(VoorzieningCB.getSelectionModel().getSelectedItem().toString().equals("Politiebureau"))
+        {
+            noodtype = 2;
+        }
+        else if(VoorzieningCB.getSelectionModel().getSelectedItem().toString().equals("Ziekenhuis")) 
+        {
+            noodtype = 3; 
+        }
+       
+            dbcon.insertLocation(VoorNaamTB.getText(), VoorLonTB.getText(), VoorLatTB.getText(), noodtype);
+       
+
     }
 }
