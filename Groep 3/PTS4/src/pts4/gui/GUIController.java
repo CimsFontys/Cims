7a7470330b72687e65dd79d2264a36a7e2a9f369
Gijs Hendrickx <305857@student.fontys.nl>
2015-06-03 11:33:24 +0200

    Uitloggen

--- Groep 3/PTS4/src/pts4/gui/GUIController.java
+++ Groep 3/PTS4/src/pts4/gui/GUIController.java
@@ -6,6 +6,7 @@
 package pts4.gui;
 
 import chat.EmergencyUnit;
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
@@ -48,6 +49,8 @@ public class GUIController implements Initializable, MapChangeListener<String, C
 
     private Administration admin;
     private Gmaps g;
+    @FXML
+    Button btnLogOut;
     @FXML
     TextField tfName;
     @FXML
@@ -346,5 +349,16 @@ public class GUIController implements Initializable, MapChangeListener<String, C
         } else if (change.wasRemoved()) {
             cbUnits.getItems().remove(change.getValueRemoved().getNaam());
         }
+    }
+    
+    public void btnLogOut_Click(ActionEvent e) throws IOException
+    {
+        Stage currentstage = (Stage) btnLogOut.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Inloggen.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));  
+        stage.show();  
+        currentstage.close();
     }
 }
